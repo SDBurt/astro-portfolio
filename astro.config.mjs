7758f153e6d55b5784e35eaabbcdb184342ddde8
sdburt <seandburt@gmail.com>
2024-06-08 16:56:52 -0700

    chore: add base files

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://astro-nano-demo.vercel.app",
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    mdx(),
+    sitemap(),
+  ],
+});
