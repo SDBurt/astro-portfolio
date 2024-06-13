48bd95c2e9948a21b5aeedd889078239d8225aa3
sdburt <seandburt@gmail.com>
2024-06-12 22:33:02 -0700

    add nothing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
